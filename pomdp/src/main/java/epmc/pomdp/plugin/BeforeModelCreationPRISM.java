7bd26172f4cb9159336556f675a2cc3d09170b93
fengwz17 <fengwz17@outlook.com>
2020-11-22 20:03:09 +0800

    plugin/pomdp

---
+++ pomdp/src/main/java/epmc/pomdp/plugin/BeforeModelCreationPRISM.java
@@ -0,0 +1,20 @@
+package epmc.pomdp.plugin;
+
+import epmc.plugin.BeforeModelCreation;
+import epmc.pomdp.value.OperatorEvaluatorPRISMPow;
+import epmc.value.operatorevaluator.SimpleEvaluatorFactory;
+
+public final class BeforeModelCreationPRISM implements BeforeModelCreation {
+    public final static String IDENTIFIER = "before-model-creation-pomdp";
+
+    @Override
+    public String getIdentifier() {
+        return IDENTIFIER;
+    }
+
+    @Override
+    public void process() {
+        SimpleEvaluatorFactory.get().add(OperatorEvaluatorPRISMPow.Builder.class);
+    }
+
+}
---
+++ pomdp/src/main/java/epmc/pomdp/plugin/Snippet.java
@@ -0,0 +1,8 @@
+package epmc.pomdp.plugin;
+
+public class Snippet {
+	public static void main(String[] args) {
+		observables
+	}
+}
+
